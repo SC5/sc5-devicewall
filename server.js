6fb38bf671e94edbf694a677ab4980193218bf0e
Henri Kostia <henri.kostia@sc5.fi>
2014-09-09 11:41:53 +0300

    Remove unused function

--- server.js
+++ server.js
@@ -133,14 +133,6 @@ var
   nsApp = io.of('/devicewallapp'),
   ns = io.of('/devicewall');
 
-function guid() {
-  function _p8(s) {
-      var p = (Math.random().toString(16)+'000000000').substr(2,8);
-      return s ? '-' + p.substr(0,4) + '-' + p.substr(4,4) : p ;
-  }
-  return _p8() + _p8(true) + _p8(true) + _p8();
-}
-
 // Namespace "devicewallapp"
 nsApp.on('connection', function (socket) {
 
