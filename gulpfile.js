f72ba0bdcbd3ba8ffa3d37a55356c78da87a35e0
Henri Kostia <henri.kostia@sc5.fi>
2014-09-16 14:55:19 +0300

    Add support for self-signed certs

--- gulpfile.js
+++ gulpfile.js
@@ -29,8 +29,8 @@ gulp.task('install', function() {
   if (!fs.existsSync('./src/app/config.js')) {
     fs.writeFileSync('./src/app/config.js', fs.readFileSync('./src/app/config.js.template'));
   }
-  gulp.src('').pipe(shell(['patch -p0 -N < foxy.patch'], {ignoreErrors: true}));
-  gulp.src('').pipe(shell(['patch -p0 -N < browsersync.patch'], {ignoreErrors: true}));
+  gulp.src('./patches/foxy/index.js').pipe(gulp.dest('./node_modules/browser-sync/node_modules/foxy'));
+  gulp.src('./patches/browser-sync/lib/server/index.js').pipe(gulp.dest('./node_modules/browser-sync/lib/server'));
 });
 
 /* Bump version number for package.json & bower.json */
