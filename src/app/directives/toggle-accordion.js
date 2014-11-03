bf65f31e559049bf5be8dee6319568f03747a4f3
Joonas Kallio <joonas@sc5.fi>
2014-11-03 16:02:54 +0200

    remove directive debug log

--- src/app/directives/toggle-accordion.js
+++ src/app/directives/toggle-accordion.js
@@ -3,7 +3,6 @@
 
   angular.module('DeviceWall').directive('toggleAccordion', ['$log', function($log) {
     return function(scope, element) {
-      $log.debug('directive ok');
       element.on('click', function() {
         $log.debug('accordion clicked');
         element.parent().toggleClass('collapsed');
