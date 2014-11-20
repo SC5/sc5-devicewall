7610066ea164597992e52a0a68b7684093a078de
Joonas Kallio <joonas@sc5.fi>
2014-11-20 15:54:49 +0200

    fix 'Bad escaping of EOL'

--- src/app/controllers/tutorial-controller.js
+++ src/app/controllers/tutorial-controller.js
@@ -7,49 +7,49 @@ angular.module('DeviceWall').controller('TutorialController', function($scope, $
     control: [
       {
         image: 'test/1.png',
-        text: '<p>Connect your device to</p>\
-              <p><span>SC5DeviceWall</span> wifi</p>\
-              <p>Password: <span>SC5Rocks</span></p>'
+        text: '<p>Connect your device to</p>' +
+              '<p><span>SC5DeviceWall</span> wifi</p>' +
+              '<p>Password: <span>SC5Rocks</span></p>'
       },
       {
         image: 'test/2.png',
-        text: '<p>Navigate to <span>http://devicewall.sc5.io</span></p>\
-              <p>and select the <span>Control panel</span> icon</p>'
+        text: '<p>Navigate to <span>http://devicewall.sc5.io</span></p>' +
+              '<p>and select the <span>Control panel</span> icon</p>'
       },
       {
         image: 'test/3.png',
-        text: '<p>Enter the website address</p>\
-              <p>you want to test</p>\
-              <p>and click <span>GO!</span></p>'
+        text: '<p>Enter the website address</p>' +
+              '<p>you want to test</p>' +
+              '<p>and click <span>GO!</span></p>'
       },
       {
         image: 'test/4.png',
-        text: '<p>Test the site and navigate</p>\
-              <p>on any of the devices</p>'
+        text: '<p>Test the site and navigate</p>' +
+              '<p>on any of the devices</p>'
       },
       {
         image: 'test/5.png',
-        text: '<p>Time to stop the test?</p>\
-              <p>Just click <span>Stop all</span></p>\
-              <p>in Control panel</p>'
+        text: '<p>Time to stop the test?</p>' +
+              '<p>Just click <span>Stop all</span></p>' +
+              '<p>in Control panel</p>'
       }
     ],
     client: [
       {
         image: 'device/1.png',
-        text: '<p>Connect your device to</p>\
-              <p><span>SC5DeviceWall</span> wifi</p>\
-              <p>Password: <span>SC5Rocks</span></p>'
+        text: '<p>Connect your device to</p>' +
+              '<p><span>SC5DeviceWall</span> wifi</p>' +
+              '<p>Password: <span>SC5Rocks</span></p>'
       },
       {
         image: 'device/2.png',
-        text: '<p>Navigate to <span>http://devicewall.sc5.io</span></p>\
-              <p>and tap <span>Add device</span> icon</p>'
+        text: '<p>Navigate to <span>http://devicewall.sc5.io</span></p>' +
+              '<p>and tap <span>Add device</span> icon</p>'
       },
       {
         image: 'device/3.png',
-        text: '<p>Give device a name</p>\
-              <p>and tap <span>OK</span>!</p>'
+        text: '<p>Give device a name</p>' +
+              '<p>and tap <span>OK</span>!</p>'
       },
       {
         image: 'device/4.png',
