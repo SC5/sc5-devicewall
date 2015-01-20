6ced1f56839f0f72608801f935a41213153987ad
Joonas Kallio <joonas@sc5.fi>
2015-01-20 18:15:10 +0200

    add check for reg exp matches

--- src/app/services/device-info.js
+++ src/app/services/device-info.js
@@ -6,28 +6,32 @@
       getPlatformInfo: function(userAgent) {
         var agent = userAgent ? userAgent : $window.navigator.userAgent;
         var matches;
+        
         if (/(iPhone|iPad)/.test(agent)) {
           matches = /(iPhone|iPad);.*OS (\d_\d)/.exec(agent);
           $log.debug('iOS matches', matches);
-          return {platform: "iOS", version: matches[matches.length-1].replace('_', '.')};
+          return {platform: "iOS", version: getVersion(matches).replace('_', '.')};
         }
 
         if (/Windows Phone/.test(agent)) {
           matches = /Windows Phone (OS )?(\d.\d);/.exec(agent);
           $log.debug('Windows phone matches', matches);
-          return {platform: "Windows Phone", version: matches[matches.length-1]};
+          return {platform: "Windows Phone", version: getVersion(matches)};
         }
 
         if (/Android/.test(agent)) {
           var re = /Android (\d+(?:\.\d+)+);/;
           matches = re.exec(agent);
           $log.debug('Android', matches);
-          return {platform: "Android", version: matches[matches.length-1]};
+          return {platform: "Android", version: getVersion(matches)};
         }
 
         return false;
       }
     };
+    function getVersion(matches) {
+      return matches[matches.length-1]?matches[matches.length-1]:'';
+    }
   });
 
 })();
